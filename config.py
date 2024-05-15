9ae9323e8105ffa1427f83a77282acac30925745
moodercoder <2017n1191@gmail.com>
2024-05-15 20:56:48 +0500

    config_file

---
+++ config.py
@@ -0,0 +1,12 @@
+CLUSTER_URL = "https://genai-project-79dbyswn.weaviate.network"
+WEAVIATE_AUTH_KEY = "hMbfwskImMEhtIZPwBsEqTCHfnN3TaAsIcaq"
+HUGGINGFACEHUB_API_TOKEN = "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"
+OPEN_AI_KEY = "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"
+
+
+PICK_PAPERS_TO_SCRAP = 2
+# to avoid API overuse errors:
+PICK_PAPERS_TO_SUMMARIZE = 1
+QUERIES_TO_PERFORM = 3
+TOP_K_SEARCH = 3
+PAPER_TO_REF = 3
